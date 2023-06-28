212c135f614734a6f64f1289a968866ccff90ab1
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-06-28 08:31:45 +0530

    Find how many object are create

---
+++ car.java
@@ -0,0 +1,11 @@
+public class car {
+    String name;
+    int number;
+    static int value=0;
+car(){
+    value++;
+}
+public void findobjnumber(){
+    System.out.println(value);
+}
+}
---
+++ num.java
@@ -0,0 +1,20 @@
+
+
+public class num {
+
+
+public static void main(String[] args) {
+    car black =new car();
+    car white=new car();
+    car green= new car();
+    car red =new car();
+    black.findobjnumber();
+}
+
+
+
+
+
+   }
+    
+   
