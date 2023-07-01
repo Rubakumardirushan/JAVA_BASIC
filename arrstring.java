be9573169c4402e2885dcc43340a1a2363cc852d
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-07-01 20:19:15 +0530

    Create arrstring.java
    Example 1:
    
    Input: nums = [10,2]
    Output: "210"
    Example 2:
    
    Input: nums = [3,30,34,5,9]
    Output: "9534330"

---
+++ arrstring.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String largestNumber(int[] nums) {
+        String s1;
+        String s2="";
+        for(int x=0;x<nums.length;x++){
+            for(int y=x+1;y<nums.length;y++){
+                if(nums[x]%10>nums[y]%10){
+                    int temp=nums[x];
+                    nums[x]=nums[y];
+                    nums[y]=temp;
+
+                }else{
+
+                }
+            }
+        }
+for(int val=nums.length-1;val>=0;val--){
+s1=Integer.toString(nums[val]);
+s2=s2+s1;
+}
+
+return s2;
+    }
+
+
+
+}
