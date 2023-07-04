44bca39d39823a253726ecb5cbefaeba9ea9939f
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-07-04 10:18:21 +0530

    Create switch.java

---
+++ switch.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class switchclz{
+
+public static void main(String args[]){
+int sizeint;
+  String sizechar;
+  Scanner Scan=new Scanner(System.in);
+  
+  System.out.println("enter your T shirt size");
+  size=Scan.Next.Int();
+switch(size){
+  case 29  ..  41:
+   sizechar="smell";
+    break;
+  case 42 .. 43:
+    sizechar="medium";
+        break;
+    case 44 .. 50:
+        sizechar="larage":
+            break:
+
+            default :
+        size="extra larage":
+            break:
+        
+
+
+
+}
+
+
+
+
+
+}
+
+
+}
