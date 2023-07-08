a4ff13661fd4c79142e12da426c9512dcb087da3
Rubakumar Dirushan <123752924+Rubakumardirushan@users.noreply.github.com>
2023-07-08 19:42:27 +0530

    Update arrstring.java

--- arrstring.java
+++ arrstring.java
@@ -5,6 +5,7 @@ class Solution {
         for(int x=0;x<nums.length;x++){
             for(int y=x+1;y<nums.length;y++){
                 if(nums[x]%10>nums[y]%10){
+                    
                     int temp=nums[x];
                     nums[x]=nums[y];
                     nums[y]=temp;
